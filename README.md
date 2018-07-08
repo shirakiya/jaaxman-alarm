a8969f4050a3b010f3f95461a3b56fddb6634d74
shirakiya <shirakiya.pv@gmail.com>
2018-07-09 03:11:55 +0900

    initial implement

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# jaaxman-alarm
-CloudWatch Alarm handler to proxy Slack
+# jaaxman-alarm-handler
+CloudWatch Alarm handler to proxy Slack.  
+  
+Using [Zappa](https://github.com/Miserlou/Zappa) flamework.
+
+
+## Environment
+- Python >= 3.6
+
+
+## SetUp
+```
+$ pip install -r requirements.txt
+```
+
+
+## Deploy
+### Preparing
+```
+$ cp jaaxman-alarm-handler-env.sample.json jaaxman-alarm-handler-env.json
+
+$ vi lambda_function_env.json  #=> write credentials
+
+$ aws s3 cp jaaxman-alarm-handler-env.json <Amazon S3 path>
+```
+
+
+### Run deploy
+```
+$ cd <repository root>
+
+$ zappa deploy
+# or Once deployed
+$ zappa update
+```
---
+++ cloudwatch_alarm.py
@@ -0,0 +1,29 @@
+import json
+import os
+
+import slackweb
+
+incomming_url = os.environ['SLACK_INCOMMING_URL']
+slack = slackweb.Slack(url=incomming_url)
+
+
+def _send_slack(title, body):
+    attachments = [{
+        'title': title,
+        'pretext': 'CloudWatch Alarm Notification',
+        'text': body,
+    }]
+
+    slack.notify(user_name='jaaxman-alarm-handler',
+                 attachments=attachments)
+
+
+def lambda_handler(event, context):
+    sns_event = event['Records'][0]['Sns']
+    message_json = sns_event['Message']
+    message = json.loads(message_json)
+
+    alarm_name = message['AlarmName']
+    new_state_reason = message['NewStateReason']
+
+    _send_slack(alarm_name, new_state_reason)
